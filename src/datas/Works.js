ef06ed45ab002224be0a37817bf2989cab4d971a
Alexis.R <rouet.alexis99@gmail.com>
2023-05-11 11:11:49 +0200

    Ajout des images, url, description

--- src/datas/Works.js
+++ src/datas/Works.js
@@ -1,52 +1,58 @@
-import React from "react";
+import avsa from "../assets/works/avsa_mobile_laptop.webp";
+import cv20 from "../assets/works/cv2.0_laptop_mobile.webp";
+import portfolio from "../assets/works/portfolio_old.webp";
+import musclefort from "../assets/works/musclefort_mobile_laptop.webp";
+import blog from "../assets/works/mockup_blog_rand.webp";
+import marionEtoile from "../assets/works/marion_etoile_laptop_mobile.webp";
 
 export const works = [
   {
     name: "avsa",
     category: "Website",
     id: "01",
-    description: "",
-    url: "",
-    cover: "",
+    description: "Réalisation d’une refonte du site d’un refuge pour chien.",
+    url: "https://avsa.alexisrouet.fr/",
+    cover: avsa,
   },
   {
     name: "Portfolio",
     category: "Website",
     id: "02",
-    description: "",
+    description: "Première portfolio à l'aide de WordPress + Elementor Pro",
     url: "https://old-cv.alexisrouet.fr/",
-    cover: "",
-  },
-  {
-    name: "Jake Gryffs",
-    category: "Prototype",
-    id: "03",
-    description: "",
-    url: "",
-    cover: "",
-  },
-  {
-    name: "Muscles Forts",
-    category: "Website",
-    id: "04",
-    description: "",
-    url: "",
-    cover: "",
-  },
-  {
-    name: "Marion Etoile",
-    category: "Prototype",
-    id: "05",
-    description: "",
-    url: "",
-    cover: "",
+    cover: portfolio,
   },
   {
     name: "CV 2.0",
     category: "Website",
-    id: "06",
-    description: "",
+    id: "02",
+    description: "Projet de formation | Concevoir un site de type CV 2.0.",
+    url: "http://cv-dev.alexisrouet.fr/",
+    cover: cv20,
+  },
+  {
+    name: "Muscles Forts",
+    category: "Website",
+    id: "03",
+    description: "Intégration d’une maquette réalisée par un camarade.",
+    url: "https://musclefort.alexisrouet.fr/",
+    cover: musclefort,
+  },
+  {
+    name: "Jake Gryffs",
+    category: "Prototype",
+    id: "04",
+    description: "Concept d’un blog sur la randonnée.",
     url: "",
-    cover: "",
+    cover: blog,
+  },
+
+  {
+    name: "Marion Etoile",
+    category: "Prototype",
+    id: "05",
+    description: "Simulation d’un cas concret.",
+    url: "",
+    cover: marionEtoile,
   },
 ];
